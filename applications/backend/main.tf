8b6eda14e22c3c1513cc226164f81087a21a4f88
Santiago Sanchez Taborda <santiago.sanchez.t@gmail.com>
2021-11-15 21:41:03 -0500

    lint(files): Add EOF line

--- applications/backend/main.tf
+++
@@ -1,7 +0,0 @@
-
-module "app-example-back" {
-	source = "../../modules/application"
-
-	app_name = "app-example-back"
-	heroku_organization = var.heroku_organization
-}
--- applications/frontend/main.tf
+++
@@ -1,6 +0,0 @@
-module "app-example-front" {
-	source = "../../modules/application"
-
-	app_name = "app-example-front"
-	heroku_organization = var.heroku_organization
-}
